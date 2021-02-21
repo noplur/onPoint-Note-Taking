7494e9a4b04b8480a32f7e9489ad099f215eb171
noplur <geoduckphoto@gmail.com>
2021-02-21 12:54:12 -0800

    update to apiRoutes notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ const { v4: uuidv4 } = require('uuid');
 const router = require("express").Router();
 const fs = require("fs");
 
-// gets the data from db.json and parses the data
+// gets the data from db.json, reads the files and parses the data
 
 router.get("/notes", (req, res) => {
     fs.readFile ("db/db.json", "UTF-8", (err, data) => {
@@ -10,11 +10,12 @@ router.get("/notes", (req, res) => {
             throw err
         }
         const notes = JSON.parse (data);
+        // function to send a response of the parameter converted to a JSON string
         res.json(notes)
     })
 });
 
-// posts data into db.json by adding a unique idea. 
+// posts data into db.json and adds a unique idea by reading the files and parsing the data then adding the id.
 
 router.post("/notes", (req, res) => {
     fs.readFile ("db/db.json", "UTF-8", (err, data) => {
@@ -25,16 +26,18 @@ router.post("/notes", (req, res) => {
         const note = req.body
         note.id = uuidv4()
         notes.push(note)
+        // writes new file with the data converted into a JSON string and a unique id
         fs.writeFile ("db/db.json", JSON.stringify(notes), (err) => {
             if (err) {
                 res.json(err);
             } 
+        // function to send a response of the parameter converted to a JSON string
         res.json(note) 
     })
 })
 });
 
-// deletes data
+// deletes data using unique id from db.json, then reads the files and splices the data
 
 router.delete("/notes/:id", (req, res) => {
     fs.readFile ("db/db.json", "UTF-8", (err, data) => {
@@ -46,8 +49,9 @@ router.delete("/notes/:id", (req, res) => {
         const noMoreID = req.params.id;
         const notes = JSON.parse (data);
         const note = req.body
-        // note.id = uuidv4()
+        // splices unique id from data under the object of noMoreID, and newly created id.
         newNote = notes.splice(noMoreID, 1)
+        // calls idTarget function
         idTarget()
         console.log(noMoreID, 0)
         // console.log(note.id, notes, note)
@@ -58,10 +62,12 @@ router.delete("/notes/:id", (req, res) => {
                 res.json(err);
             } 
 
-        // Send response
+        // function to send a response of the new parameter converted to a JSON string
         res.json(notes)
 
     })
+
+    // function idTarget that converts id into whole numbers starting with 0, 1, 2, etc.
     function idTarget() {
         for (i = 0; i < notes.length; i ++) {
             notes[i].id = i;
